4fd78de082aea93aa2066e56025da1df8db88e58
10358337766 <58505460+10358337766@users.noreply.github.com>
2019-12-04 16:12:50 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
+define('APIKEY', getenv("AIzaSyCUZkk01CZlqJ61DRUDDdaUZyC4UkQvGvo"));
 define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
+define('SITE_NAME',getenv("木木的转接站"));
+define('TITLENAME', getenv("木木的转接站"));
 define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('EMAIL', getenv("zlj1035833776@gmail.com"));
 ?>
